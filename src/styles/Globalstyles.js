eaef585236030e9f0bd0f1e3a688edfbb7d7fab0
장형원 <janghyung-won@janghyeong-won-ui-MacBookAir.local>
2022-05-10 14:51:15 +0900

    Modify: 초기세팅 finally done

--- src/styles/Globalstyles.js
+++ src/styles/Globalstyles.js
@@ -2,8 +2,8 @@ import { createGlobalStyle } from 'styled-components';
 import reset from 'styled-reset';
 
 const GlobalStyles = createGlobalStyle` 
-  ${reset} // styled-reset이라는 패키지를 설치해야한다. 몇가지만 reset해 줄 경우 사용하지 않아도 무방하다.
-
+  ${reset} 
+  
   @font-face {
     font-family: 'GmarketSansMedium';
     src: url('https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_2001@1.1/GmarketSansMedium.woff') format('woff');
@@ -37,10 +37,6 @@ const GlobalStyles = createGlobalStyle`
 
     body {
       font-family: "GmarketSansMedium";
-    }
-
-    h1 {
-      font-family: 'EarlyFontDiary';
     }
 
     button {
